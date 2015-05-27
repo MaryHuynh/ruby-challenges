5bfab10c867a1697b0114d33af976c604a00aff0
Sophie <mh9sd@virginia.edu>
2015-05-27 01:32:05 -0400

    commmit for branch_practice_file.rb

---
+++ branch_practice_file.rb
@@ -0,0 +1,6 @@
+This is my file for practicing with Git.
+
+"This is some content to fill the page."
+"People probably don't read these things, huh?"
+"We can make it all strings. No functions here!"
+
